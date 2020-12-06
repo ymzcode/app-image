ba9ef524112009671c5e55fd05319a620d9a254b
吃饱了不饿 <68903346+ymzcode@users.noreply.github.com>
2020-12-06 21:25:57 +0800

    添加一些测试案例

--- pages/test/test-1.vue
+++ pages/test/test-1.vue
@@ -8,10 +8,16 @@
 		<app-image src="https://img.cdn.aliyun.dcloud.net.cn/uni-app/doc/run1x9.jpg" isCatch></app-image>
 		<text>图片3(错误图片地址)</text>
 		<app-image src="https://img.cdn.aliyun.dcloud.net.cn/uni-app/doc/xxxxxxxx.jpg" isCatch></app-image>
-		<text>图片4(设置固定宽高 300)</text>
-		<app-image src="https://img.cdn.aliyun.dcloud.net.cn/uni-app/doc/run1x9.jpg" width="300" height="300"></app-image>
-		<text>图片5(关闭自动调整图片尺寸)</text>
+		<text>图片4(设置固定宽高 300, 关闭缓存)</text>
+		<app-image src="https://img.cdn.aliyun.dcloud.net.cn/uni-app/doc/run1x9.jpg" width="300" height="300" :isCatch="false"></app-image>
+		<text>图片5(关闭自动调整图片尺寸 不设置宽高默认为 88rpx)</text>
 		<app-image src="https://img.cdn.aliyun.dcloud.net.cn/uni-app/doc/run1x9.jpg" :isProportion="false"></app-image>
+		<text>图片6(关闭自动调整图片尺寸 设置宽高为 200rpx)</text>
+		<app-image src="https://img.cdn.aliyun.dcloud.net.cn/uni-app/doc/run1x9.jpg" :isProportion="false" width="200" height="200"></app-image>
+		<text>图片7(开启自动调整图片尺寸 设置宽高为 200rpx)</text>
+		<app-image src="https://img.cdn.aliyun.dcloud.net.cn/uni-app/doc/run1x9.jpg" width="200" height="200"></app-image>
+		<text>图片8(关闭图片自检时 缓存\自动调整图片尺寸\压缩图片等扩展功能失效 默认大小88rpx)</text>
+		<app-image src="https://img.cdn.aliyun.dcloud.net.cn/uni-app/doc/run1x9.jpg" :autoCheckImage="false"></app-image>
 	</view>
 </template>
 
--- pages/test/test-3.vue
+++ pages/test/test-3.vue
@@ -3,9 +3,10 @@
 		<text>图片1(原图, 没有开启 缓存&压缩质量, 大小1.4M - 1468006.4 字节)</text>
 		<app-image src="https://desk-fd.zol-img.com.cn/t_s2880x1800c5/g5/M00/03/0C/ChMkJ1XSyouIZWwEABdcKRk9iD8AAAPKAIq4ZUAF1xB614.jpg"></app-image>
 		
-		<text>图片2(开启压缩质量 当前30, 大小通过方法getCompressImageInfo可以获取)</text>
-		<text>我这边测试大概在444632字节 大约434KB</text>
-		<app-image src="https://desk-fd.zol-img.com.cn/t_s2880x1800c5/g5/M00/03/0C/ChMkJ1XSyouIZWwEABdcKRk9iD8AAAPKAIq4ZUAF1xB614.jpg" compressQuality="20" @getCompressImageInfo="getCompressImageInfo"></app-image>
+		<text>图片2(开启压缩质量 当前30, 大小通过方法getCompressImageInfo可以获取)\n</text>
+		<text>测试大概在444632字节 大约434KB\n</text>
+		<text>测试发现开启后 加载图片会变慢一些</text>
+		<app-image src="https://desk-fd.zol-img.com.cn/t_s2880x1800c5/g5/M00/03/0C/ChMkJ1XSyouIZWwEABdcKRk9iD8AAAPKAIq4ZUAF1xB614.jpg" compressQuality="30" @getCompressImageInfo="getCompressImageInfo"></app-image>
 	</div>
 </template>
 
